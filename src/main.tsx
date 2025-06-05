872a8cca2eb7c5939797eb92546483aee58eecdb
Tomas Zingis <thebash2344@gmail.com>
2025-06-05 17:32:11 +0200

    fixed-css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./index.css";
 import App from "./components/App/App.tsx";
 
 const queryClient = new QueryClient();
